7b0ed658945e8d7386b7341cac991b9c44b03413
Arthur Gishyan <gishyan.a25@gmail.com>
2026-07-08 15:50:23 +0400

    Added Human validation

--- src/main/java/com/arthur/education/Main.java
+++ src/main/java/com/arthur/education/Main.java
@@ -2,25 +2,13 @@ package com.arthur.education;
 
 public class Main {
     public static void main(String[] args) {
-        helloPrinter("Hello", "Arthur");
-        divOfTwo(8,4);
+        Human human = new Human();
 
+        human.setName("*");
+        System.out.println(human.getName());
 
-        System.out.println("Sum : " + sumOfTwo(5,6));
-
-
-
-    }
-
-    public static void helloPrinter(String message, String userName) {
-        System.out.println(message + "\n" + userName);
-    }
-
-    public static int sumOfTwo(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    public static void divOfTwo(int num1, int num2) {
-        System.out.println("Division : " + (float)(num1 / num2));
+        human.lastName = "Gishyan";
+        human.gender = "Male";
+        human.age = 19;
     }
 }
